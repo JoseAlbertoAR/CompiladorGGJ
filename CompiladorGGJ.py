a70e4d5f783d4a628b22ced941d1aeaf64a4f6ba
Albert812 <106777143+JoseAlbertoAR@users.noreply.github.com>
2024-04-09 23:26:30 -0600

    Yaaaa

--- CompiladorGGJ.py
+++ CompiladorGGJ.py
@@ -2,54 +2,71 @@
 #Jose Alberto Alvarado Rosales
 #Biblioteca para leer el archivo de texto
 import re
+from tabulate import tabulate
 
 # Definir tokens
 token_patterns = [
-    (r'\b(var|while|if|else|do|main|prints|println|reads?)\b', 'KEYWORD'),
+    (r'\b(while|if|else|do|main|brake|inc|prints|println|reads?)\b', 'KEYWORD'),
     (r'\b(add|get|set|inc|dec|reverse|size)\b', 'FUNCTION'),
-    (r'\b(num|option|start|finish|temp|result|remainder)\b', 'VARIABLE'),
     (r'\b(true|false)\b', 'BOOLEAN'),
     (r'\b(\d+)\b', 'INTEGER_CONSTANT'),
     (r'[+\-*/%=]', 'OPERATOR'),
     (r'==|!=|>=|<=|\=\>', 'COMPARISON_OPERATOR'),
-    (r'\(', 'OPEN_PARENTHESIS'),
-    (r'\)', 'CLOSE_PARENTHESIS'),
-    (r'\[', 'OPEN_BRACKET'),
-    (r'\]', 'CLOSE_BRACKET'),
-    (r'{', 'OPEN_BRACE'),
-    (r'}', 'CLOSE_BRACE'),
-    (r'\.', 'DOT_OPERATOR'),
-    (r'".*?"', 'STRING_LITERAL'),
-    (r'<#[\s\S]*?#>', 'COMMENT'),
-    (r';', 'SEMICOLON'),
+    (r'!', 'LOGICAL_NOT'),
+    (r'&&', 'LOGICAL_AND'),
+    (r'\|\|', 'LOGICAL_OR'),
+    (r'\^', 'LOGICAL_XOR'),
+    (r'\b\w+´\b', 'SINGLE_QUOTE'),
+    (r'(?<!\\)".*?(?<!\\)"', 'STRING_LITERAL'),
+    (r'(?<!\\)\'', 'SINGLE_QUOTE'),
+    (r'<#([\s\S]*?)#>', 'COMMENT'),
+    (r'(?<!\\)\\r', 'CARRIAGE_RETURN'),
+    (r';', 'SEMICOLON'), 
+    (r'\bvar\s+\w+\b', 'VARIABLE'), 
     (r'[a-zA-Z_]\w*', 'IDENTIFIER')
 ]
+
 def read_text_from_file(filename):
     with open(filename, 'r') as file:
         return file.read()
 
-# Identifica los tokens en el texto
 def tokenize(text):
     tokens = []
+    is_comment = False  
+    variables = set()  
     for pattern, token_type in token_patterns:
         regex = re.compile(pattern)
         matches = regex.finditer(text)
         for match in matches:
-            tokens.append((token_type, match.group()))
-    return tokens
+            token = match.group()
+            if token_type == 'COMMENT':
+                tokens.append((token_type, match.group(1))) 
+                is_comment = True
+                break 
+            if not is_comment:
+                if token_type == 'VARIABLE':
+                    variables.add(token.split()[1])  
+                tokens.append((token_type, token))
+    return tokens, variables
 
 filename = input("Por favor, ingrese el nombre del archivo: ")
 
 try:
-
     texto = read_text_from_file(filename)
+    tokens, variables = tokenize(texto)
 
-    # Divide el texto en los tokens
-    tokens = tokenize(texto)
+    # Imprimir los tokens y emparejarlos con los que se definieron
+    table_headers = ["Tipo de Token", "Token"]
+    table_data = [(token_type, token) for token_type, token in tokens]
+    print(tabulate(table_data, headers=table_headers, tablefmt="grid"))
 
-    # Imprimir los tokens y emparejarlos con los que se definierons
-    for token in tokens:
-        print(f"Token: {token[1]} (Tipo: {token[0]})")
+    # Mostrar las variables encontradas
+    if variables:
+        print("\nVariables encontradas:")
+        for var in variables:
+            print(var)
+    else:
+        print("\nNo se encontraron variables.")
 
 except FileNotFoundError:
     print(f"El archivo '{filename}' no se encontró.")
